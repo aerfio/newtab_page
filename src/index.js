be989f2069a135aa7505a6592306e5b16bf8e730
aerfio <mati6095@gmail.com>
2017-10-31 12:17:51 +0100

    small change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Terminal from './components/terminal/terminal'
 class Square extends React.Component {
     render() {
         let personalTab = Array.of('Personal', 'Fagbook', 'Onet', 'ImgSearch', 'Trello', 'dumblr');
-        let personalLinks = Array.of('https://www.facebook.com/home.php', 'http://www.onet.pl', 'https://images.google.com/', 'https://trello.com/b/v9HDsFRa/2do', 'http://mywallofnoise.tumblr.com/');
+        let personalLinks = Array.of('https://www.facebook.com/home.php', 'http://www.onet.pl', 'https://images.google.com/', 'https://trello.com/b/v9HDsFRa/2do', 'http://tumblr.com/');
 
         let stuffTab = Array.of('Stuff', '/wg/', '/mu/', 'Google Pogoda', 'Google Drive', 'Studia rau1');
         let stuffLinks = Array.of('https://boards.4chan.org/wg/', 'https://boards.4chan.org/mu/', 'https://www.google.pl/#q=pogoda', 'https://drive.google.com/drive/my-drive', 'https://platforma.polsl.pl/rau1/course/index.php?categoryid=26');
