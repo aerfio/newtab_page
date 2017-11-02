24fd55705faacf9f1a9b90bc329e89f92d6e7f31
aerfio <mati6095@gmail.com>
2017-11-02 14:07:31 +0100

    weather json is in console also 4chan redirect works from searchbar

---
+++ src/components/terminal/weather.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import request from 'request';
+
+
+class Weather extends React.Component {
+    render() {
+        let apiKey = 'af6eccccbe9e42573102a3f5a16ceb48';
+        let city = 'Gliwice';
+        let url = 'http://api.openweathermap.org/data/2.5/find?q=' + city + '&units=metric&appid=' + apiKey;
+        request.get(url, function (err, res, body) {
+            if (err) {
+                console.log('error:');
+            } else {
+                console.log('body:', body);
+            }
+        });
+
+
+        return (
+            <div>
+                lol
+            </div>
+
+        );
+
+
+    }
+}
+
+export default Weather;
