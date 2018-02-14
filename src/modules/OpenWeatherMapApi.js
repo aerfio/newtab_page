faffa5312ac25e96a392d27acd9d6253497f28ce
aerfio <mati6095@gmail.com>
2018-02-14 23:03:29 +0100

    created openweathermapapi.js

---
+++ src/modules/OpenWeatherMapApi.js
@@ -0,0 +1,9 @@
+const apiKey='your key here';
+const city='your city here'
+module.exports = {
+     apiKey : apiKey,
+     city : city,
+     urlCur : 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + apiKey,
+   urlFive:  'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&appid=' + apiKey,
+    gym: 'Mydło\nAntyperspirant\nSzampon\nRęcznik + ręcznik mały\nButy\nStrój\nSkarpetki + baty\nWoda\nPortfel\nKłódka\nDokumenty + klucze do samochodu',
+};
