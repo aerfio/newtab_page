94bbdc16f9944ed2aee49e40babfd28007d610f4
aerfio <mati6095@gmail.com>
2018-02-14 23:01:46 +0100

    created links.js

---
+++ src/modules/links/links.js
@@ -0,0 +1,13 @@
+module.exports = {
+    one: Array.of('Link #1','https://www.google.pl/'),
+    two: Array.of('Link #2','https://www.google.pl/'),
+    three: Array.of('Link #3','https://www.google.pl/'),
+    four: Array.of('Link #4','https://www.google.pl/'),
+    five: Array.of('Link #5','https://www.google.pl/'),
+    six: Array.of('Link #6','https://www.google.pl/'),
+    seven: Array.of('Link #7','https://www.google.pl/'),
+    eight: Array.of('Link #7','https://www.google.pl/'),
+    nine: Array.of('Link #9','https://www.google.pl/'),
+    ten: Array.of('Link #10','https://www.google.pl/'),
+    headers : Array.of('Header #1','Header #2'),
+};
